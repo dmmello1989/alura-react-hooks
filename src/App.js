f013022494af43ed0e3beb52a6a054ae7949bba9
Daniel Mello <dmmello1989@gmail.com>
2020-12-07 19:21:22 -0300

    conclusão do curso

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
+import 'fontsource-roboto';
+import { Container, Typography } from '@material-ui/core';
 import FormularioCadastro from './components/formularioCadastro';
 
 function App() {
+  const onSubmit = (data) => {
+    console.log({data})
+  }
+
+  const validarCPF = (cpf) => {
+    if(cpf.length !== 11) {
+      return { valid: false, text: "CPF deve ser composto por 11 dígitos" }
+    } else if(!Number(cpf)) {
+      return { valid: false, text: "CPF deve ser composto apenas por números" }
+    } else {
+      return { valid: true }
+    }
+  }
+
   return (
-    <div className="App">
-      <h1>Formulário de cadastro</h1>
-      <FormularioCadastro />
-    </div>
+    <Container component="article" maxWidth="sm">
+      <Typography variant="h3" component="h1" >
+        Formulário de cadastro
+      </Typography>
+      <FormularioCadastro onSubmit={onSubmit} validarCPF={validarCPF} />
+    </Container>
   );
 }
 
--- src/components/formularioCadastro/index.js
+++ src/components/formularioCadastro/index.js
@@ -1,21 +1,80 @@
-import Button from "@material-ui/core/Button";
+import { useState } from "react";
+import { TextField, Button, Switch, FormControlLabel } from "@material-ui/core";
 
-function FormularioCadastro() {
-  return(
-    <form>
-      <label>Nome</label>
-      <input type="text" />
-      <label>Sobrenome</label>
-      <input type="text" />
-      <label>CPF</label>
-      <input type="text" />
+function FormularioCadastro({ onSubmit, validarCPF }) {
+  const [nome, setNome] = useState("");
+  const [sobrenome, setSobrenome] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [promocoes, setPromocoes] = useState(true);
+  const [novidades, setNovidades] = useState(true);
+  const [erros, setErros] = useState({cpf: { valid: true, text: "" }});
 
-      <label>Promoções</label>
-      <input type="checkbox"/>
-      <label>Novidades</label>
-      <input type="checkbox"/>
+  return (
+    <form 
+      onSubmit={e => {
+        e.preventDefault();
+        onSubmit({ nome, sobrenome, cpf, promocoes, novidades });
+      }}
+    >
+      <TextField
+        id="Nome" 
+        fullWidth 
+        label="Nome" 
+        value={nome}
+        margin="normal" 
+        variant="outlined" 
+        onChange={e => setNome(e.target.value)}
+      />
+      <TextField 
+        fullWidth 
+        id="Sobrenome" 
+        margin="normal" 
+        label="Sobrenome" 
+        value={sobrenome}
+        variant="outlined" 
+        onChange={e => setSobrenome(e.target.value)}
+      />
+      <TextField 
+        id="CPF" 
+        fullWidth 
+        label="CPF" 
+        value={cpf}
+        margin="normal" 
+        variant="outlined" 
+        error={!erros.cpf.valid}
+        onBlur={e => {
+          const isValid = validarCPF(cpf);
+          setErros({cpf: isValid});
+        }}
+        onChange={e => setCpf(e.target.value)}
+        helperText={erros.cpf.text}
+      />
 
-      <Button type="submit" variant="container" color="primary">Cadastrar</Button>
+      <FormControlLabel
+        label="Promoções"
+        control={
+          <Switch 
+            color="primary" 
+            name="promoções" 
+            checked={promocoes}
+            onChange={e => setPromocoes(e.target.checked)}
+          />
+        }
+      />
+
+      <FormControlLabel
+        label="Novidades"
+        control={
+          <Switch 
+            color="primary" 
+            name="novidades" 
+            checked={novidades}
+            onChange={e => setNovidades(e.target.checked)}
+          />
+        }
+      />
+
+      <Button type="submit" variant="contained" color="primary">Cadastrar</Button>
     </form>
   )
 }
